c0c2e6890ec536acbccb8ce41a57d456f0be5730
martaexpositocamejo <martaexpositocamejo@gmail.com>
2026-02-15 18:06:48 +0000

    tercer_cambio

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 pg_intro = st.Page('intro.py', title='Introducción')
-pg_eda_intro = st.Page('seccion_eda/introduccion.py', title='Intro EDA')
+pg_eda_intro = st.Page('seccion_eda/introduccion.py', title='Primeros Pasos')
 
 navigation_env = st.navigation(
     {
-        '''Inicio''': [pg_intro],
+        '''''': [pg_intro],
         '''EDA''': [pg_eda_intro]
     }
 )
---
+++ seccion_eda/intro_eda.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Primeros Pasos para el EDA del alquiler de bicis')
+
